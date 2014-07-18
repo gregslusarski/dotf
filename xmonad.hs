1a58a197aa6d39c7fd13d09a7492f8a0e29018ce
viszu <viszu84@gmail.com>
2014-07-18 04:03:38 +0200

    Update

--- xmonad.hs
+++ xmonad.hs
@@ -1,14 +1,18 @@
 import XMonad
 import XMonad.Util.Run (spawnPipe)
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
 
 main = do
   xmproc <- spawnPipe "/usr/bin/xmobar /home/viszu/.xmobarrc"
   xmonad $ defaultConfig
-    { terminal    = myTerminal
+    { manageHook  = manageDocks <+> manageHook defaultConfig
+    , layoutHook  = avoidStruts  $  layoutHook defaultConfig
+    , terminal    = myTerminal
     , modMask     = myModMask
     , borderWidth = myBorderWidth
     }
 
 myTerminal    = "urxvt"
 myModMask     = mod4Mask -- Win key or Super_L
-myBorderWidth = 1
+myBorderWidth = 0
