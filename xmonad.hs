a40e2fd43d952c15c4f606bff706e36cbd2bad05
viszu <viszu84@gmail.com>
2014-07-19 01:31:23 +0200

    Update

--- xmonad.hs
+++ xmonad.hs
@@ -3,6 +3,10 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.EZConfig (additionalKeys)
 
+myTerminal    = "urxvt"
+myModMask     = mod4Mask -- Win key or Super_L
+myBorderWidth = 0
+
 main = do
   xmonad $ defaultConfig
     { manageHook  = manageDocks <+> manageHook defaultConfig
@@ -10,9 +14,9 @@ main = do
     , terminal    = myTerminal
     , modMask     = myModMask
     , borderWidth = myBorderWidth
-    } `additionalKeys`
-    [ ((mod4Mask,  xK_p  ), spawn "dmenu_run -fn Cantarell-11 -nb black -nf '#ebdbb2'") ]
+    } `additionalKeys` myKeys
 
-myTerminal    = "urxvt"
-myModMask     = mod4Mask -- Win key or Super_L
-myBorderWidth = 0
+myKeys =
+  [ ((mod4Mask,  xK_p  ), spawn "dmenu_run -fn Cantarell-11 -nb black -nf '#ebdbb2'")
+  , ((shiftMask .|. mod4Mask,  xK_l  ), spawn "slimlock")
+  ]
