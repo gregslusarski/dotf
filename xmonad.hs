309a15f8afed05ec61c9c4044c7f0b70ce5ab39b
viszu <viszu84@gmail.com>
2014-07-30 22:45:06 +0200

    Update

--- xmonad.hs
+++ xmonad.hs
@@ -10,24 +10,25 @@ myNormalBorderColor = "#000000"
 myFocusedBorderColor = "#99ccff"
 
 myManageHook = composeAll
-   -- Open in workspace "x"
-   [ className =? "Pidgin" --> doShift "2"
-   , className =? "Spotify" --> doShift "2"
-   , className =? "Rhythmbox" --> doShift "2"
-   , className =? "Transmission-gtk" --> doShift "2"
-   , className =? "Google-chrome-stable" --> doShift "2"
-   -- Float those
-   , className =? "Viewnior" --> doFloat
-   , className =? "MuPDF" --> doFloat
-   , className =? "Gvim" --> doFloat
-   , className =? "Vlc" --> doFloat
-   , manageDocks
-   ]
+  -- Open in workspace "x"
+  [ className =? "Pidgin" --> doShift "2"
+  , className =? "Spotify" --> doShift "2"
+  , className =? "Rhythmbox" --> doShift "2"
+  , className =? "Transmission-gtk" --> doShift "2"
+  , className =? "Google-chrome-stable" --> doShift "2"
+  , className =? "Googleearth-bin" --> doShift "2"
+  -- Float those
+  , className =? "Viewnior" --> doFloat
+  , className =? "MuPDF" --> doFloat
+  , className =? "Gvim" --> doFloat
+  , className =? "Vlc" --> doFloat
+  , manageDocks
+  ]
 
 main = do
   xmonad $ defaultConfig
     { manageHook = myManageHook <+> manageHook defaultConfig
-    , layoutHook = avoidStruts  $  layoutHook defaultConfig
+    , layoutHook = avoidStruts $ layoutHook defaultConfig
     , terminal = myTerminal
     , modMask = myModMask
     , borderWidth = myBorderWidth
@@ -39,5 +40,6 @@ myKeys =
   [ ((mod4Mask,  xK_p  ), spawn "dmenu_run -fn 'Roboto Medium-10' -nb black -nf '#ebdbb2'")
   , ((shiftMask .|. mod4Mask,  xK_l  ), spawn "slimlock")
   , ((mod4Mask , xK_Print ), spawn "scrot -q 100 ~/pics/screens/%Y-%m-%d-%H-%M-%S.png")
+  , ((mod4Mask,  xK_Delete  ), spawn "xkill" )
   , ((mod4Mask,  xK_b  ), sendMessage ToggleStruts)
   ]
